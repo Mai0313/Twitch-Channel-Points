9188dc9b5746454f332d10161d819314b066a7fb
A1z2s3x4566 <weichenglee0313@gmail.com>
2023-12-13 03:06:41 +0800

    Add Matrix and Pushover integrations

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from TwitchChannelPointsMiner.classes.entities.Bet import (
     Strategy,
 )
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
+from TwitchChannelPointsMiner.classes.Matrix import Matrix
+from TwitchChannelPointsMiner.classes.Pushover import Pushover
 from TwitchChannelPointsMiner.classes.Settings import Events, FollowersOrder, Priority
 from TwitchChannelPointsMiner.classes.Telegram import Telegram
 from TwitchChannelPointsMiner.logger import ColorPalette, LoggerSettings
@@ -72,7 +74,10 @@ def get_miner():
         logger_settings=LoggerSettings(
             save=config.save,
             console_level=logging.INFO,  # Level of logs - use logging.DEBUG for more info
-            file_level=logging.DEBUG,  # Level of logs - If you think the log file it's too big, use logging.INFO
+            console_username=False,  # Adds a username to every console log line if True. Also adds it to Telegram, Discord, etc. Useful when you have several accounts
+            auto_clear=True,  # Create a file rotation handler with interval = 1D and backupCount = 7 if True (default)
+            time_zone="",  # Set a specific time zone for console and file loggers. Use tz database names. Example: "America/Denver"
+            file_level=logging.INFO,  # Level of logs - If you think the log file it's too big, use logging.INFO
             emoji=check_platform(),
             less=config.less,
             colored=config.colored,
@@ -84,7 +89,12 @@ def get_miner():
             telegram=Telegram(
                 chat_id=config.telegram.chat_id,
                 token=config.telegram.token,
-                events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE, "BET_LOSE"],
+                events=[
+                    Events.STREAMER_ONLINE,
+                    Events.STREAMER_OFFLINE,
+                    Events.BET_LOSE,
+                    Events.CHAT_MENTION,
+                ],  # Only these events will be sent to the chat
                 disable_notification=config.telegram.disable_notification,
             ),
             discord=Discord(
@@ -93,13 +103,33 @@ def get_miner():
                     Events.STREAMER_ONLINE,
                     Events.STREAMER_OFFLINE,
                     Events.BET_LOSE,
+                    Events.CHAT_MENTION,
                 ],  # Only these events will be sent to the chat
+            ),
+            matrix=Matrix(
+                username="twitch_miner",  # Matrix username (without homeserver)
+                password="...",  # Matrix password
+                homeserver="matrix.org",  # Matrix homeserver
+                room_id="...",  # Room ID
+                events=[
+                    Events.STREAMER_ONLINE,
+                    Events.STREAMER_OFFLINE,
+                    Events.BET_LOSE,
+                ],  # Only these events will be sent to the chat
+            ),
+            pushover=Pushover(
+                userkey="YOUR-ACCOUNT-TOKEN",  # Login to https://pushover.net/, the user token is on the main page.
+                token="YOUR-APPLICATION-TOKEN",  # Create a application on the website, and use the token shown in your application.
+                priority=0,  # Read more about priority here: https://pushover.net/api#priority
+                sound="pushover",  # A list of sounds can be found here: https://pushover.net/api#sounds
+                events=[Events.CHAT_MENTION, Events.DROP_CLAIM],  # Only these events will be sent.
             ),
         ),
         streamer_settings=StreamerSettings(
             make_predictions=config.bet_strategy.make_predictions,
             follow_raid=config.bet_strategy.follow_raid,
             claim_drops=config.bet_strategy.claim_drops,
+            claim_moments=True,  # If set to True, https://help.twitch.tv/s/article/moments will be claimed when available
             watch_streak=config.bet_strategy.watch_streak,
             chat=ChatPresence.ONLINE,
             bet=BetSettings(
@@ -129,6 +159,8 @@ def get_miner():
 # For example, if in the mine function you don't provide any value for 'make_prediction' but you have set it on TwitchChannelPointsMiner instance, the script will take the value from here.
 # If you haven't set any value even in the instance the default one will be used
 
+# twitch_miner.analytics(host="127.0.0.1", port=5000, refresh=5, days_ago=7)   # Start the Analytics web-server
+
 if __name__ == "__main__":
     twitch_miner = get_miner()
     twitch_miner.mine(
@@ -136,3 +168,93 @@ if __name__ == "__main__":
         followers=True,  # Automatic download the list of your followers
         followers_order=FollowersOrder.ASC,  # Sort the followers list by follow date. ASC or DESC
     )
+    # twitch_miner.mine(
+    #     [
+    #         Streamer(
+    #             "streamer-username01",
+    #             settings=StreamerSettings(
+    #                 make_predictions=True,
+    #                 follow_raid=False,
+    #                 claim_drops=True,
+    #                 watch_streak=True,
+    #                 bet=BetSettings(
+    #                     strategy=Strategy.SMART,
+    #                     percentage=5,
+    #                     stealth_mode=True,
+    #                     percentage_gap=20,
+    #                     max_points=234,
+    #                     filter_condition=FilterCondition(
+    #                         by=OutcomeKeys.TOTAL_USERS, where=Condition.LTE, value=800
+    #                     ),
+    #                 ),
+    #             ),
+    #         ),
+    #         Streamer(
+    #             "streamer-username02",
+    #             settings=StreamerSettings(
+    #                 make_predictions=False,
+    #                 follow_raid=True,
+    #                 claim_drops=False,
+    #                 bet=BetSettings(
+    #                     strategy=Strategy.PERCENTAGE,
+    #                     percentage=5,
+    #                     stealth_mode=False,
+    #                     percentage_gap=20,
+    #                     max_points=1234,
+    #                     filter_condition=FilterCondition(
+    #                         by=OutcomeKeys.TOTAL_POINTS, where=Condition.GTE, value=250
+    #                     ),
+    #                 ),
+    #             ),
+    #         ),
+    #         Streamer(
+    #             "streamer-username03",
+    #             settings=StreamerSettings(
+    #                 make_predictions=True,
+    #                 follow_raid=False,
+    #                 watch_streak=True,
+    #                 bet=BetSettings(
+    #                     strategy=Strategy.SMART,
+    #                     percentage=5,
+    #                     stealth_mode=False,
+    #                     percentage_gap=30,
+    #                     max_points=50000,
+    #                     filter_condition=FilterCondition(
+    #                         by=OutcomeKeys.ODDS, where=Condition.LT, value=300
+    #                     ),
+    #                 ),
+    #             ),
+    #         ),
+    #         Streamer(
+    #             "streamer-username04",
+    #             settings=StreamerSettings(make_predictions=False, follow_raid=True, watch_streak=True),
+    #         ),
+    #         Streamer(
+    #             "streamer-username05",
+    #             settings=StreamerSettings(
+    #                 make_predictions=True,
+    #                 follow_raid=True,
+    #                 claim_drops=True,
+    #                 watch_streak=True,
+    #                 bet=BetSettings(
+    #                     strategy=Strategy.HIGH_ODDS,
+    #                     percentage=7,
+    #                     stealth_mode=True,
+    #                     percentage_gap=20,
+    #                     max_points=90,
+    #                     filter_condition=FilterCondition(
+    #                         by=OutcomeKeys.PERCENTAGE_USERS, where=Condition.GTE, value=300
+    #                     ),
+    #                 ),
+    #             ),
+    #         ),
+    #         Streamer("streamer-username06"),
+    #         Streamer("streamer-username07"),
+    #         Streamer("streamer-username08"),
+    #         "streamer-username09",
+    #         "streamer-username10",
+    #         "streamer-username11",
+    #     ],  # Array of streamers (order = priority)
+    #     followers=False,  # Automatic download the list of your followers
+    #     followers_order=FollowersOrder.ASC,  # Sort the followers list by follow date. ASC or DESC
+    # )
